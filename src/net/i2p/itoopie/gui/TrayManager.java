891c40e402181996442136c038998bf9d1969e88
dev <dev@robertfoss.se>
2011-07-15 09:47:53 +0000

    Changed MouseListener into MouseAdapter.

--- src/net/i2p/itoopie/gui/TrayManager.java
+++ src/net/i2p/itoopie/gui/TrayManager.java
@@ -9,6 +9,7 @@ import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.net.URL;
@@ -57,33 +58,15 @@ public class TrayManager {
 		            trayIcon = new TrayIcon(IconLoader.getTrayImage(), "itoopie", getMainMenu());
 		            trayIcon.setImageAutoSize(true); //Resize image to fit the system tray
 		            
-		            trayIcon.addMouseListener(new MouseListener(){
-		
+		            trayIcon.addMouseListener(new MouseAdapter(){
 						@Override
 						public void mouseClicked(MouseEvent arg0) {
 							WindowHandler.toggleFrames();
 						}
-		
-						@Override
-						public void mouseEntered(MouseEvent arg0) {				
-						}
-		
-						@Override
-						public void mouseExited(MouseEvent arg0) {			
-						}
-		
-						@Override
-						public void mousePressed(MouseEvent arg0) {				
-						}
-		
-						@Override
-						public void mouseReleased(MouseEvent arg0) {				
-						}
 		            });
 		            try {
 		                tray.add(trayIcon);
-		            } catch (AWTException e) {
-		            }
+		            } catch (AWTException e) {}
 		        }
     		}
     	});

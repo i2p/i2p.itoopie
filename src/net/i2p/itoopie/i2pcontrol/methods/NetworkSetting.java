2a704af52e0f2c584a1c64386df8771185444ad9
dev <dev@robertfoss.se>
2011-07-15 09:59:25 +0000

    Refactoring.

--- src/net/i2p/itoopie/i2pcontrol/methods/NetworkSetting.java
+++ src/net/i2p/itoopie/i2pcontrol/methods/NetworkSetting.java
@@ -28,20 +28,61 @@ public class NetworkSetting{
 	 * @author hottuna
 	 */
 	public enum NETWORK_SETTING implements Remote{
-		DETECTED_IP { 	public boolean isReadable(){ return true;}	public boolean isWritable(){ return false;} public String toString() { return "i2p.router.net.ssu.detectedip"; }},
-		TCP_PORT { 		public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.ntcp.port"; }},
-		TCP_HOSTNAME { 	public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.ntcp.hostname"; }},
-		TCP_AUTOIP { 	public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.ntcp.autoip"; }},
-		UDP_PORT { 		public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.ssu.port"; }},
-		UDP_HOSTNAME { 	public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.ssu.hostname"; }},
-		UDP_AUTO_IP { 	public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.ssu.autoip"; }},
-		UPNP { 			public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.upnp"; }},
-		BW_SHARE { 		public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.bw.share"; }},
-		BW_IN { 		public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.bw.in"; }},
-		BW_OUT { 		public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.bw.out"; }},
-		LAPTOP_MODE { 	public boolean isReadable(){ return true;}	public boolean isWritable(){ return true;}  public String toString() { return "i2p.router.net.laptopmode"; }},
-		SETTINGS_SAVED {public boolean isReadable(){ return false;}	public boolean isWritable(){ return false;} public String toString() { return "SettingsSaved";}},
-		RESTART_NEEDED {public boolean isReadable(){ return false;}	public boolean isWritable(){ return false;} public String toString() { return "RestartNeeded";}}
+		DETECTED_IP { 	public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return false;} 
+						public String toString() { return "i2p.router.net.ssu.detectedip"; }},
+						
+		TCP_PORT { 		public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.ntcp.port"; }},
+						
+		TCP_HOSTNAME { 	public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.ntcp.hostname"; }},
+						
+		TCP_AUTOIP { 	public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.ntcp.autoip"; }},
+						
+		UDP_PORT { 		public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.ssu.port"; }},
+						
+		UDP_HOSTNAME { 	public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.ssu.hostname"; }},
+						
+		UDP_AUTO_IP { 	public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.ssu.autoip"; }},
+						
+		UPNP { 			public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.upnp"; }},
+						
+		BW_SHARE { 		public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.bw.share"; }},
+						
+		BW_IN { 		public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.bw.in"; }},
+						
+		BW_OUT { 		public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.bw.out"; }},
+						
+		LAPTOP_MODE { 	public boolean isReadable(){ return true;}	
+						public boolean isWritable(){ return true;}  
+						public String toString() { return "i2p.router.net.laptopmode"; }},
+						
+		SETTINGS_SAVED {public boolean isReadable(){ return false;}	
+						public boolean isWritable(){ return false;} 
+						public String toString() { return "SettingsSaved";}},
+						
+		RESTART_NEEDED {public boolean isReadable(){ return false;}	
+						public boolean isWritable(){ return false;} 
+						public String toString() { return "RestartNeeded";}}
 	};
 		
 	static {
